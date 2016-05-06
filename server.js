6cb11a396497b22e2cdb6cf7cc787a966386d43f
Stephen <stephenlallum@gmail.com>
2016-05-05 16:11:21 -1000

    get task route implemented

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 
 const express = require('express'),
       app     = express(),
-      PORT    = process.env.PORT || 3000
+      PORT    = process.env.PORT || 3000,
+      taskRoute = require('./routes/task')
       ;
 
 app.use(express.static('public'));
-
+app.use('/task', taskRoute);
 
 app.listen(PORT, function(){
   console.log(`Server listening on port ${PORT}`);
